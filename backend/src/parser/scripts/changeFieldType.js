e89336aac9236fe2bcdc4fe09f97628c60603133
Nils Streijffert <nils.streijffert@gmail.com>
2020-04-08 15:41:58 +0200

    Add script to set type for fields

---
+++ backend/src/parser/scripts/changeFieldType.js
@@ -0,0 +1,29 @@
+const { client } = require('../../elastic/handler')
+
+const changeFieldType = ({ changeObject, index }) => client.indices
+    .putMapping({
+      index,
+      body: {
+        properties: {
+          ...changeObject
+        }
+      }
+    })
+    .then((result) => console.log(JSON.stringify(result, null, 2)))
+    .catch((error) => console.error(JSON.stringify(error.meta.body.error, null, 2)))
+
+
+changeFieldType({
+  changeObject: {
+    agency: {
+      type: 'keyword'
+    },
+    oganisationNumber: {
+      type: 'keyword'
+    },
+    createdAt: {
+      type: 'date'
+    }
+  },
+  index: ['arsredovisningar', 'regleringsbrev']
+})
