181f2678157e2f40f73392308fd8fe951a74c64e
Nils Streijffert <nils.streijffert@gmail.com>
2020-04-13 09:39:22 +0200

    Add prod settings for Heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const body_parser = require('body-parser')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const api = require('./api/')
 
 app.use(body_parser.json())
