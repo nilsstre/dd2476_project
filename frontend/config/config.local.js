48dc0cc2f7add1746e1ddd03e758d9051337ac5d
Nils Streijffert <nils.streijffert@gmail.com>
2020-04-14 11:41:09 +0200

    Fix path to backend

--- frontend/config/config.local.js
+++ frontend/config/config.local.js
@@ -1,5 +1,4 @@
 module.exports = {
   backend_URL: 'http://localhost:8080',
-  PATH_TO_FILES: './src/general/data/pdf/',
   S3_ADDRESS: 'https://dd2476-project.s3.eu-north-1.amazonaws.com/'
 }
--- frontend/config/config.prod.js
+++ frontend/config/config.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  backend_URL: 'http://backend.evilfriends.se',
+  backend_URL: 'https://backend.evilfriends.se',
   S3_ADDRESS: 'https://dd2476-project.s3.eu-north-1.amazonaws.com/'
 }
