bc3d0e3e10a6764f8308d599de6d9ee33456b601
Nils Streijffert <nils.streijffert@gmail.com>
2020-05-08 17:25:49 +0200

    Migrate to Heroku

--- frontend/config/config.prod.js
+++ frontend/config/config.prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  backend_URL: 'https://backend.evilfriends.se',
+  backend_URL: 'https://dd2476-backend.herokuapp.com',
   S3_ADDRESS: 'https://dd2476-project.s3.eu-north-1.amazonaws.com/'
 }
